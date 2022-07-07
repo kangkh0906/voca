b0d6ccd1644cde1b30b56063f9877a3b2ff96538
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-07-07 15:14:27 +0900

    [UPDATE] Day 삭제 기능

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,14 +1,46 @@
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate, Navigate} from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import Word from './Word';
+import {useState} from 'react';
+import DayList from './DayList';
 
 function Day() {
-  const { day } = useParams();
+  const a = useParams();
+  const day = a.day;
+  const days = useFetch(`http://localhost:3001/days?day=${day}`);
   const words = useFetch(`http://localhost:3001/words?day=${day}`);
+  let word;
+  const [_word, set_Word] = useState(word);
+  const [_day, set_Day] = useState(days);
+  const navigate = useNavigate();
+
+  function dayDel() {
+    console.log(days[0].day);
+    if (window.confirm('삭제하시겠습니까?')) {
+      fetch(`http://localhost:3001/days/${days[0].id}`, {
+        method: 'DELETE'
+      }).then(res => {
+        set_Day({ id:0 });
+      });
+      for (word of words) {
+        fetch(`http://localhost:3001/words/${word.id}`, {
+          method: 'DELETE'
+        }).then(res => {
+          set_Word({ id:0 });
+        })
+      }
+      alert('삭제가 완료되었습니다.');
+      navigate(`/`);
+      }
+  }
+
+  if (day[0].id === 0) {
+    return null;
+  }
 
   return(
     <div>
-      <h2>Day {day}</h2>
+      <h2 className='day_header'>Day {day} <button className='btn_del' onClick={dayDel}>Day 삭제</button></h2>
       {words.length === 0 && (<span>Loading...</span>)}
       <table>
         <tbody>
