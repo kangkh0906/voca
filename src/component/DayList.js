9960af8349c8124a16ef84ba683a1467cf020dcd
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-07-07 16:29:06 +0900

    [UPDATE] DayList를 Day 순으로 정렬

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -11,7 +11,9 @@ function DayList() {
   return (
     <div>
       <ul className="list_day">
-        {days.map(day => (
+        {days
+          .sort((a, b) => a.day - b.day)
+          .map(day => (
           <li key={day.id}>
             <Link to={`/day/${day.day}`}>Day {day.day}</Link>
           </li>
