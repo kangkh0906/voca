50dac1c17419e1f68bcee87e6229b3e01ace4717
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-07-07 16:31:51 +0900

    [UPDATE] Add Tab To Increase Readability

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -14,10 +14,11 @@ function DayList() {
         {days
           .sort((a, b) => a.day - b.day)
           .map(day => (
-          <li key={day.id}>
-            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-          </li>
-        ))}
+            <li key={day.id}>
+              <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
